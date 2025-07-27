f9ba6c799e670e23b2bfb5f41cdeda2df33608b9
sahanpramudith <pramuditsahan466@gmail.com>
2025-07-27 19:18:52 +0530

    Add TextBox class with Selenium test for input field

---
+++ src/test/java/TextBox.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TextBox {
+
+
+    WebDriver driver;
+    @BeforeMethod
+    public void TestBox(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.leafground.com/input.xhtml");
+    }
+
+    @Test
+    public void TextTypeBoxTest(){
+        //Type your name
+        WebElement name = driver.findElement(By.id("j_idt88:name"));
+        name.sendKeys("sahan");
+
+
+    }
+}
